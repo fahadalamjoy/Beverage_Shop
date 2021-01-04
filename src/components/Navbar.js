1da60bbd0cf89c918d169603492b6855efe1f0ff
fahadalamjoy <61037426+fahadalamjoy@users.noreply.github.com>
2021-01-04 16:46:03 +0600

    Files Uploaded

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <h1>The Beverage Shop</h1>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about">about</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
